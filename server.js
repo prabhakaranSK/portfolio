d809ba6bad621a4cd0d8d1d4c90038f6dfd38eed
prabhakaranSK <prabhakaransv69@gmail.com>
2025-11-08 11:37:13 +0530

    comiitted

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ app.use(express.json());
 
 app.use(
   cors({
-    origin: ["http://localhost:5173"],
+    origin: ["http://localhost:5173", "https://portfolio-web-mocha-eta.vercel.app/"],
     methods: ["POST"],
   })
 );
